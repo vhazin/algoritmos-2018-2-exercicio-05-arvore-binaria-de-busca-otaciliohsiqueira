fb3fedb847cfa739b6acf6a77b39d819117a7e62
Otacílio Siqueira <38449763+otaciliohsiqueira@users.noreply.github.com>
2018-09-16 22:24:23 -0300

    Quase_lá

---
+++ ohs.c
@@ -0,0 +1,114 @@
+#include <stdio.h>
+
+
+typedef struct Node 
+{
+
+    int valor;    
+    struct Node *esq;
+    struct Node *dir;
+
+}Tree;
+
+
+// Criando uma arvore
+void createTree(Tree **root)
+{
+    *root = NULL;
+}
+
+// Inserir uma nova raiz
+Tree* insertRoot(Tree *root, int valor)
+{
+    root->valor = valor;
+    root->esq = NULL;
+    root->dir = NULL;
+
+    return root;
+}
+
+// Inserir na arvore
+int insertTree(Tree *root, int valor)
+{
+    if(root == NULL)  
+    { 
+        root = insertRoot(root, valor);
+        return root;
+    }
+    else if(valor <= root->valor)
+    {
+        root->esq = insertTree(root->esq, valor);
+    }
+    else
+    {
+        root->dir = insertTree(root->dir, valor);
+    }
+}
+
+
+void inOrdem(Tree *root)
+{
+    if(root != NULL)
+    {
+        inOrdem(root->esq);
+        printf("\n%i", root->valor);
+        inOrdem(root->dir);
+    }
+}
+
+void preOrdem(Tree *root)
+{
+    if(root != NULL)
+    {
+        printf("\n%i", root->valor);
+        preOrdem(root->esq);
+        preOrdem(root->dir);
+    }
+}
+
+void posOrdem(Tree *root)
+{
+    if(root != NULL)
+    {
+        posOrdem(root->esq);
+        posOrdem(root->dir);
+        printf("\n%i", root->valor);
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    Tree *binaryTree = NULL;
+    int qtdTestes;
+    int qtdNumeros;
+    int valores;
+    scanf("%d", &qtdTestes);
+    for(int i = 0; i < qtdTestes; i++)
+    {
+        scanf("%d", &qtdNumeros);
+        Tree *aux = binaryTree;
+        binaryTree = NULL;
+
+        for(int j = 0; j < qtdNumeros; j++)
+        {
+            scanf("%d ", &valores);
+            binaryTree = insertTree(binaryTree, valores);
+        }
+
+        printf("Case %d:\n", i);
+
+        printf("\nPre.: ");
+        preOrdem(valores);
+
+        printf("\nIn..: ");
+        inOrdem(valores);
+
+        printf("\nPost: ");
+        posOrdem(valores);
+
+        printf("\n\n");
+    }
+
+
+    return 0;
+}
