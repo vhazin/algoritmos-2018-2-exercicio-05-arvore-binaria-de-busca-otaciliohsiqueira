edb3af82770537e5fb5dfcdece267fa25b3bf2a0
Otacílio Siqueira <38449763+otaciliohsiqueira@users.noreply.github.com>
2018-09-16 23:35:08 -0300

    FINAL

--- ohs.c
+++ ohs.c
@@ -12,30 +12,16 @@ typedef struct Node
 }Tree;
 
 
-// Criando uma arvore
-int createTree(Tree **root)
-{
-    *root = NULL;
-    return root;
-}
-
-// Inserir uma nova raiz
-Tree* insertRoot(Tree *root, int valor)
-{
-    Tree *auxRoot = malloc(sizeof(Tree));
-    auxRoot->valor = valor;
-    auxRoot->esq = NULL;
-    auxRoot->dir = NULL;
-
-    return auxRoot;
-}
-
 // Inserir na arvore
 Tree* insertTree(Tree *root, int valor)
 {
     if(root == NULL)  
     { 
-        return insertRoot(createTree(valor), valor);
+        Tree *auxRoot = malloc(sizeof(Tree));
+        auxRoot->valor = valor;
+        auxRoot->esq = NULL;
+        auxRoot->dir = NULL;
+        return auxRoot;
         
     }
     else if(valor <= root->valor)
@@ -81,7 +67,7 @@ void posOrdem(Tree *root)
 
 int main(int argc, char const *argv[])
 {
-    Tree *binaryTree = NULL;
+    
     int qtdTestes;
     int qtdNumeros;
     int valores;
@@ -89,26 +75,30 @@ int main(int argc, char const *argv[])
     for(int i = 0; i < qtdTestes; i++)
     {
         scanf("%d", &qtdNumeros);
-        Tree *aux = binaryTree;
-        binaryTree = NULL;
-        free(aux);
+        
+        Tree * binaryTree = NULL;
+        
 
         for(int j = 0; j < qtdNumeros; j++)
         {
-            scanf("%d ", &valores);
+            scanf("%d", &valores);
             binaryTree = insertTree(binaryTree, valores);
         }
 
-        printf("Case %d:\n", i);
+        printf("Case %d:\n", i+1);
 
-        printf("\nPre.: ");
-        preOrdem(valores);
-
-        printf("\nIn..: ");
-        inOrdem(valores);
-
-        printf("\nPost: ");
-        posOrdem(valores);
+        printf("Pre.:");
+        preOrdem(binaryTree);
+        
+        
+        printf("\n");
+        printf("In..:");
+        inOrdem(binaryTree);
+        
+        
+        printf("\n");
+        printf("Post:");
+        posOrdem(binaryTree);
 
         printf("\n\n");
     }
